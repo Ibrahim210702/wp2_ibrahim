80c77a98fe6bbcba9931a4a4011f5b4a17e50ecf
Ibrahim210702 <baimlancer@gmail.com>
2021-09-22 22:57:45 +0700

    upload

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ibrahim
+ Saya tinggal di daerah Bekasi
+ olahraga yang saya sukai adalah
+ Berenang";
+    }
+}
